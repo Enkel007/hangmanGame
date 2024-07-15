bd24fee86428286c5523a0aa009329db5613cf51
Enkel007 <eshehdula22@epoka.edu.al>
2024-07-15 18:49:06 +0200

    window frame set

--- src/Hangman.java
+++ src/Hangman.java
@@ -1,2 +1,12 @@
-public class Hangman {
+import javax.swing.*;
+
+public class Hangman extends JFrame {
+    public Hangman(){
+        super("Hangman Game (Java Ed.");
+        setSize(CommonConstants.FRAME_SIZE);
+        setDefaultCloseOperation(EXIT_ON_CLOSE);
+        setLocationRelativeTo(null);
+        setLayout(null);
+        setResizable(false);
+    }
 }

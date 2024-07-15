80767ea603bf6c609e7d1fdfb341294c9f6a1cd1
Enkel007 <eshehdula22@epoka.edu.al>
2024-07-15 19:26:55 +0200

    added GUI component (hangman image)

--- src/Hangman.java
+++ src/Hangman.java
@@ -1,6 +1,16 @@
 import javax.swing.*;
 
 public class Hangman extends JFrame {
+    //counts the number of incorrect guesses made by the player
+    private int incorrectGuesses;
+
+    //store the challenge from the WordDB here
+    private String[] wordChallenge;
+
+    private final WordDB wordDB;
+
+    private JLabel hangmanImage;
+
     public Hangman(){
         super("Hangman Game (Java Ed.");
         setSize(CommonConstants.FRAME_SIZE);
@@ -8,5 +18,19 @@ public class Hangman extends JFrame {
         setLocationRelativeTo(null);
         setLayout(null);
         setResizable(false);
+
+        //initiating vars
+        wordDB = new WordDB();
+        wordChallenge = wordDB.loadChallenge();
+
+        addGUIComponents();
+    }
+
+    private void addGUIComponents(){
+        //hangman image
+        hangmanImage = CustomTools.loadImage(CommonConstants.IMAGE_PATH);
+        hangmanImage.setBounds(0, 0, hangmanImage.getPreferredSize().width, hangmanImage.getPreferredSize().height);
+
+        getContentPane().add(hangmanImage);
     }
 }
